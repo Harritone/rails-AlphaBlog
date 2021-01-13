f45456b833f6d3e697290cdb3e4e20bde99c6f46
Harritone <yourrasik@gmail.com>
2021-01-13 18:18:17 +1000

    Moved aplication helpers to application controller to be accessed from controllers as well as from views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,7 @@
 module ApplicationHelper
+
   def gravatar_for(user, options = { size: 80 })
     image_tag("http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{options[:size]}", alt: user.username, class: 'shadow rounded mx-auto d-block')
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  
-  def logged_in?
-    # session[user_id] ? true : false
-    !!current_user
   end
 
 end
