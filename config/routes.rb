ec6de3e031a0f36286d8ae0c9014611a484f23e0
Harritone <yourrasik@gmail.com>
2021-01-14 20:19:20 +1000

    Built categories controller and actions using functional tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  resources :users, expect: [:new]  
+
+  resources :users, except: [:new]  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, expect: [:destroy]
 end
