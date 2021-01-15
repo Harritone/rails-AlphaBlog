eed34054db9a11ff0a8e1a9fde8eb1968a271e4c
Harritone <yourrasik@gmail.com>
2021-01-15 11:38:12 +1000

    Added admin user requirement for creation category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: 'honk', email: 'honk@mail.com', 
+    password: '123456', admin: true)
+  end
   test "getting new for and create category" do
+    sign_in_as(@admin_user)
     get '/categories/new'
     assert_response :success 
     assert_difference 'Category.count', 1 do
@@ -14,6 +19,7 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
   end
 
   test "getting new for and reject invalid category submission" do
+    sign_in_as(@admin_user)
     get '/categories/new'
     assert_response :success 
     assert_no_difference 'Category.count'  do
