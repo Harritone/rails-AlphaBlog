7598ad95e54122d2ebfa58abbcf6978773ce9d2d
Harritone <yourrasik@gmail.com>
2021-01-15 17:51:51 +1000

    Implemented edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,12 @@
 class CategoriesController < ApplicationController
-  before_action :require_admin, except: [:index, :show]
+  before_action :require_admin, except: [:index, :show ]
+  before_action :set_category, only: [:show, :edit, :update]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -25,11 +25,29 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit 
+  end
+
+  def update
+      if @category.update(category_params)
+        flash[:notice] = "Category was successfully updated"
+        redirect_to @category
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+
   def category_params
     params.require(:category).permit(:name)
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def require_admin
     if !(logged_in? && current_user.admin?)
